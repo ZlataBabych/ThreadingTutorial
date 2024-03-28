84f0e5c7a86ce3a0af0f0fc4c0fc6bfe2f459a0e
Zlata Babych <126006978+ZlataBabych@users.noreply.github.com>
2024-03-28 15:33:13 +0200

    Add project files.

---
+++ ThreadingTutorial.cpp
@@ -0,0 +1,22 @@
+﻿// ThreadingTutorial.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <thread>
+#include <chrono>  //time
+
+using namespace std;
+
+int main()
+{
+	cout << this_thread::get_id() << endl;
+	this_thread::sleep_for(chrono::seconds(3));
+	cout << this_thread::get_id() << endl;
+	
+
+
+
+
+
+	return 0;
+}
